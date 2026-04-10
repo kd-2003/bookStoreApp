e5d94f1c2b2d84f66e50a4eeb111aca74ec92841
Kuldeep-Prajapati <kumarkuldeep56000@gmail.com>
2026-04-10 23:58:07 +0530

    Update FreeBook.jsx

--- frontend/src/components/FreeBook.jsx
+++ frontend/src/components/FreeBook.jsx
@@ -14,7 +14,7 @@ function FreeBook() {
     useEffect(() => {
     const getBook = async()=>{
       try{
-        const res = await axios.get("http://localhost:8080/book");
+        const res = await axios.get("https://github.com/kd-2003/document-system/book");
         console.log(res.data);
         const data = res.data.filter((data) => data.category === "Free");
         setBook(data);
